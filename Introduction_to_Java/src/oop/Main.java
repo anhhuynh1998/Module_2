378a7034dc4f6d202c4186b4434805fcc08cd4d4
anhhuynh1998 <anhprono1998@gmail.com>
2023-04-17 14:31:08 +0700

    update

---
+++ Introduction_to_Java/src/oop/Main.java
@@ -0,0 +1,17 @@
+package oop;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("nhập chiều rộng :");
+        double width = scanner.nextDouble();
+        System.out.print("nhập chiều cao :");
+        double height = scanner.nextDouble();
+        Rectangle rectangle = new Rectangle(width,height);
+        System.out.println("diện tích là :" + rectangle.getArea());
+        System.out.println("chu vi là :" + rectangle.getPerimeter());
+        System.out.println("Your Rectangle \n" + rectangle.display());
+    }
+}
